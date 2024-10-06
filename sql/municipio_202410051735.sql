6dada5b46ce8bea1a885a3ca3406d3cec1191c86
staubjoao <joao.staub42@gmail.com>
2024-10-06 14:21:21 -0300

    feat sql municipio

---
+++ sql/municipio_202410051735.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.municipio (municipio_id,infestado,municipio,quantidade_habitantes,quantidade_imoveis_rurais,quantidade_imoveis_urbanos) VALUES
+	 ('08efa49f-77c6-4db0-a41b-8478651df549'::uuid,true,'MARINGÁ',10000,10000,10000),
+	 ('fa44971a-c1e1-4d2c-b0ee-0bacc7a691fb'::uuid,false,'NOVA ESPERANÇA',10000,10000,1000);
